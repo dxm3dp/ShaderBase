a1ad59cf911ace6eac04bb77097f3c4752a46b52
晓萌 杜 <dxm3dp@gmail.com>
2021-02-17 22:46:47 +0800

    添加PostEffect目录

---
+++ ShaderBase/Assets/PostEffect/PostEffect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public sealed class PostEffect : MonoBehaviour
+{
+    [Tooltip("The shader for blur pass")]
+    Shader blurPassShader;
+    [Tooltip("whether to enable the bloom")]
+    bool enableBloom;
+    [Tooltip("The bloom blend mode")]
+    BloomBlendMode bloomBlendMode = BloomBlendMode.Add;
+    [Tooltip("The bloom intensity")]
+    float bloomIntensity = 0.5f;
+    [Tooltip("The bloom threshold")]
+    [Range(-0.05f, 4f)]
+    float bloomThreshold = 0.5f;
+    [Tooltip("The bloom threshold color")]
+    Color bloomThresoldColor = Color.white;
+    [Range(0f, 10f)]
+    float blurSpread = 2.5f;
+
+    Material blurPassMaterial;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    { }
+
+    enum BloomBlendMode
+    {
+        // Blend the bloom with the image using screen mode
+        Screen = 0,
+        // Blend the bloom with the image using add mode
+        Add = 1,
+    }
+}
